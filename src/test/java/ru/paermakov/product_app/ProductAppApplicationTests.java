e81649f98beadee06300766463b967029f2a5ebb
peterermakow <peterinno@yandex.ru>
2024-05-06 16:27:29 +0300

    created new application

---
+++ src/test/java/ru/paermakov/product_app/ProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.paermakov.product_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
